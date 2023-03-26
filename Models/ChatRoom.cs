45edd0d0708d1012f53218287c05fe6abbee1528
ownims <ownims@gmail.com>
2023-03-26 19:33:11 -0300

    FirstCommit

---
+++ Models/ChatRoom.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Chatolino.Models {
+    public class ChatRoom {
+        public string Name { get; set; }
+        public List<User> Users { get; set; }
+        public List<Message> Messages { get; set; }
+
+        public ChatRoom(string name) {
+            Name = name;
+            Users = new List<User>();
+            Messages = new List<Message>();
+        }
+
+        public void AddUser(User user) {
+            Users.Add(user);
+        }
+
+        public void RemoveUser(User user) {
+            Users.Remove(user);
+        }
+
+        public void AddMessage(Message message) {
+            Messages.Add(message);
+        }
+    }
+}
---
+++ Models/Message.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Chatolino.Models {
+    public class Message {
+        public int Id { get; set; }
+        public string Sender { get; set; }
+        public string Text { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
